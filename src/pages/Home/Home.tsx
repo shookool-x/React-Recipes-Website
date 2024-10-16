2ecc647a1e402ff07f4f5105b424154d64a461ab
shklx <amirrostami.b1994@gmail.com>
2024-10-16 23:32:14 +0330

    use firebase in Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,53 @@
 import './Home.css';
-import useFetch from '../../hooks/useFetch';
 import SkeletLoding from '../../components/SkeletLoding';
 import { FoodItem } from '../../models/Interfaces';
 import RecipeList from '../../components/RecipeList';
+import { useEffect, useState } from 'react';
+
+import { db } from '../../firebase/config';
+import { collection, getDocs } from 'firebase/firestore';
 
 
 
 export default function Home() {
 
-  const { data, error, isLoading } = useFetch<FoodItem[]>('http://localhost:4000/recipes');
+  const [data, setData] = useState<FoodItem[]>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    const ref = collection(db, 'recipes');
+    getDocs(ref)
+      .then((snapshot) => {
+        if (snapshot.empty) {
+          setError('No Item To Show');
+          setIsLoading(false);
+        } else {
+
+          const result: FoodItem[] = [];
+
+          snapshot.docs.forEach(doc => {
+            const fooditem: FoodItem = {
+              id: doc.id,
+              title: doc.data().title || '',
+              ingredients: doc.data().ingredients || [],
+              method: doc.data().method || '',
+              cookingTime: doc.data().cookingTime || '',
+            }
+            result.push(fooditem);
+          })
+          setData(result);
+          setIsLoading(false);
+        }
+
+      })
+  
+  }, [])
+
+
   return (
 
     <div className='home'>
